eea30130042f39c4b3ee72c609a2e00527970a8a
Gerba <gmlongobardi@hotmail.com>
2024-09-13 17:27:46 -0300

    V7.4

--- app/[locale]/barcelona/page.js
+++ app/[locale]/barcelona/page.js
@@ -4,6 +4,7 @@ import styles from '../../ui/components/Barcelona/barcelona.module.css';
 import MapBarcelona from '@/app/ui/components/Barcelona/MapBarcelona/MapBarcelona';
 import DataBarcelona from '@/app/ui/components/Barcelona/DataBarcelona/DataBarcelona';
 import Contact from '@/app/ui/components/Home/Contact/Contact';
+import Script from 'next/script';
 
 
 
@@ -16,6 +17,7 @@ const Barcelona = () => {
       <DataBarcelona />
       <MapBarcelona />
       <Contact section={'barcelona'} />
+      <Script src={`https://maps.googleapis.com/maps/api/js?key=${process.env.GOOGLE_MAPS_API_KEY}&loading=async&libraries=map,marker,places`} strategy="beforeInteractive"></Script>
     </div>
   )
 }
